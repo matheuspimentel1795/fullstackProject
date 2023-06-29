034bc4e3b7ce2958ec0bc32986a8da7d975f3dcb
matheus <matheus.freire@meta.com.br>
2023-06-29 13:24:23 -0300

    ajuste front

---
+++ front/portifolio/src/pages/ContactHome/Header/index.js
@@ -0,0 +1,25 @@
+import { Grid, TextField } from "@mui/material"
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import SearchIcon from '@mui/icons-material/Search';
+import { useState } from "react";
+export const HeaderContacts = () => {
+    const [filter,setFilter] = useState(false)
+    const changeFilter = () =>{
+        setFilter(!filter)
+    }
+    return(
+        <Grid sx={{backgroundColor: filter? 'white' :'#128C7E'}} padding='2%' >
+            {filter ? 
+            <Grid display={'flex'} alignItems='center' >
+                <ArrowBackIcon onClick={changeFilter} />
+            <TextField sx={{backgroundColor:'white'}} fullWidth variant="standard"  />
+            </Grid>
+            : 
+            <Grid item xs={12}  display={'flex'} alignItems='center' justifyContent={'space-between'}>
+            <h4>Contatos</h4>
+            <SearchIcon onClick={changeFilter}/>
+            </Grid>}
+        </Grid>
+    )
+}
--- front/portifolio/src/pages/ContactHome/index.js
+++ front/portifolio/src/pages/ContactHome/index.js
@@ -2,10 +2,11 @@ import TextField from '@mui/material/TextField';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { useState,useEffect } from 'react';
 import { Button, Grid } from '@mui/material';
-
+import CreateIcon from '@mui/icons-material/Create';
 import { useForm, FormProvider, useFormContext } from "react-hook-form"
 import getCon, { api, getContacts, postContact } from '../../service';
 import {  useNavigate } from 'react-router-dom';
+import { HeaderContacts } from './Header';
 
 export const ContactHome = () =>{
     const [telNumber,setTelNumber] = useState()
@@ -31,10 +32,19 @@ export const ContactHome = () =>{
       },[])
     
     return (
-        <Grid>
-            {userList.map((item)=>{
+        <Grid container collums={12}>
+            <Grid item xs={12}>
+                <HeaderContacts/>
+            </Grid>
+            {userList?.map((item)=>{
                 return(
+                    <Grid sx={{border:'1px solid black'}} item xs={12} display={'flex'} alignItems={'center'} justifyContent={'space-between'}>
+                       <Grid item xs ={5} display={'flex'}  >
+                        <AccountCircleIcon sx={{width: '100px',height:'100px'}}/>
                     <h1>{item.nome}</h1>
+                    </Grid>
+                    <CreateIcon/>
+                    </Grid>
                 )
             })}
         </Grid>
